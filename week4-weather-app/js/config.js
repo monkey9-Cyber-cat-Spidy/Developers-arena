3b4dba4b3d614b957d5698e172d54c4b11b6a492
monkey9-Cyber-cat-Spidy <spidy0904mani@gmail.com>
2026-01-03 15:24:51 +0530

    config.js update

--- week4-weather-app/js/config.js
+++ week4-weather-app/js/config.js
@@ -4,7 +4,7 @@
 // Never expose real production keys this way.
 
 // Change this value to your actual Weatherstack API access key.
-const WEATHERSTACK_ACCESS_KEY = '0198accc871ca009a3b0d8c8fc6d04b6';
+const WEATHERSTACK_ACCESS_KEY = 'your_weatherstack_api_key_here';
 
 // Weatherstack base URL
 const WEATHERSTACK_BASE_URL = 'https://api.weatherstack.com';
